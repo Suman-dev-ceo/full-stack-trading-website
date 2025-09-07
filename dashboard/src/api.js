ca5b05b5f986a59c3b8eae9a607d5627753731bd
Suman Srinivas <sumansrinivas@Sumans-MacBook-Pro.local>
2025-09-07 17:22:43 +0530

    modifying api.js

--- dashboard/src/api.js
+++ dashboard/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+axios.defaults.baseURL = "/api";
 axios.defaults.withCredentials = true; // send cookies
-axios.defaults.baseURL =
-  process.env.REACT_APP_API_URL || "http://localhost:8080"; // Render URL in Netlify
+// axios.defaults.baseURL =
+//   process.env.REACT_APP_API_URL || "http://localhost:8080"; // Render URL in Netlify
 
 export default axios;
